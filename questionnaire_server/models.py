466d0644bcc83e4fa9d1bb04afdb9d7eecba84c2
MichaelStriegel <mstriege@kent.edu>
2013-10-14 11:00:13 -0400

    Patient model added

--- questionnaire_server/models.py
+++ questionnaire_server/models.py
@@ -19,7 +19,18 @@ class Question(models.Model):
 class UserAnswer(models.Model):
     answer = models.CharField(max_length=500)
     question = models.ForeignKey(Question)
+    patient = models.ForeignKey('Patient')
 
     def __unicode__(self):
         return self.answer
 
+class Patient(models.Model):
+    patient_ID = models.CharField(max_length=100, primary_key=True)
+    password = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
+    notes = models.TextField(blank=True)
+	
