ee74f6ffe828a0788b7ab5eb86662ea284c876b2
Camden Fullmer <camdenfullmer@gmail.com>
2013-10-30 03:57:04 -0400

    Updated wsgi file.

--- questionnaire_server/wsgi.py
+++ questionnaire_server/wsgi.py
@@ -20,7 +20,7 @@ import sys
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "questionnaire_server.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "questionnaire_server.settings")
-sys.path.append(' /var/www/create.cs.kent.edu/questionnaire-server')
+sys.path.append(' /var/www/create.cs.kent.edu')
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
