578eba0d9a353fbb542897ab77e9fdd798ca86d7
Camden Fullmer <camdenfullmer@gmail.com>
2013-11-01 23:10:26 -0400

    Added absolute path to static files.'

--- questionnaire_server/settings.py
+++ questionnaire_server/settings.py
@@ -78,7 +78,7 @@ SERIALIZATION_MODULES = {
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/create.cs.kent.edu/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
--- questionnaire_server/wsgi.py
+++ questionnaire_server/wsgi.py
@@ -19,10 +19,6 @@ import sys
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "questionnaire_server.settings"
-sys.path.append('/var/www/create.cs.kent.edu/questionnaire_server')
-sys.path.append('/var/www/create.cs.kent.edu')
-for path in sys.path: print path
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "questionnaire_server.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
