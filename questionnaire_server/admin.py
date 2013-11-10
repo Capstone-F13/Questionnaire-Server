1d7a5d90b97a58d8c220881d869b85f460b476c6
danielgur <danielgur2105@gmail.com>
2013-11-10 15:25:08 -0500

    customizing list views in admin to view user responses #42

--- questionnaire_server/admin.py
+++ questionnaire_server/admin.py
@@ -2,10 +2,13 @@ from questionnaire_server.models import MultipleChoiceAnswer, Question, Patient,
 
 from django.contrib import admin
 
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'question', 'answer')
+
 admin.site.register(MultipleChoiceAnswer)
 admin.site.register(Question)
 admin.site.register(Patient)
 admin.site.register(Study)
 admin.site.register(Survey)
-admin.site.register(UserAnswer)
+admin.site.register(UserAnswer, UserAnswerAdmin)
 
