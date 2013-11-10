61cedc770827aa7d9dc00de1957b89abfe97e302
danielgur <danielgur2105@gmail.com>
2013-11-10 03:13:32 -0500

    cleaning the code and removed unnecessary params in add patient - we could get the admin id from the access token #41

--- questionnaire_server/views.py
+++ questionnaire_server/views.py
@@ -26,18 +26,18 @@ def submit_answer(request):
                 answer_text = MultipleChoiceAnswer.objects.get(pk=answer_id)
 
             if not answer_text:
-                return HttpResponse(simplejson.dumps({ "error" : "answer/answer_id field is required!" }))
+                return _generate_response({ "error" : "answer/answer_id field is required!" })
 
             # TODO: check if the answer already exists
             user_answer = UserAnswer(answer=answer_text, question=question, patient=patient)
             user_answer.save()
 
         except KeyError as e:
-            return HttpResponse(simplejson.dumps({ "error" : "Malformed data!", "message" : e }))
-        return HttpResponse(simplejson.dumps({ "success" : "Answer submission successful" }))
+            return _generate_response({ "error" : "Malformed data!", "message" : e })
+        return _generate_response({ "success" : "Answer submission successful" })
 
     else:
-        return HttpResponse(simplejson.dumps({ "error" : "Request needs to post json" }))
+        return _generate_response({ "error" : "Request needs to post json" })
 
 
 def get_patients(request, token=None):
@@ -45,10 +45,10 @@ def get_patients(request, token=None):
 
     if request.method == 'GET':
         if not token:
-            return HttpResponse(simplejson.dumps({ "error" : "Request needs a token" }))
+            return _generate_response({ "error" : "Request needs a token" })
         access_token = AccessToken.objects.filter(token=token)
         if not access_token:
-            return HttpResponse(simplejson.dumps({ "error" : "Access Token is not valid" }))
+            return _generate_response({ "error" : "Access Token is not valid" })
         user = access_token[0].user
         patients = Patient.objects.filter(administrators=user)
 
@@ -56,9 +56,9 @@ def get_patients(request, token=None):
         for patient in patients:
             patient_ids.append(patient.patient_id)
 
-        return HttpResponse(simplejson.dumps({ "patient_ids" : patient_ids }), mimetype="application/json")
+        return _generate_response({ "patient_ids" : patient_ids })
     else:
-        return HttpResponse(simplejson.dumps({ "error" : "Expecting a GET request" }))
+        return _generate_response({ "error" : "Expecting a GET request" })
 
 
 def login_patient(request):
@@ -69,14 +69,13 @@ def login_patient(request):
             patient_id = json_data['patient_id']
             access_token = json_data['access_token']
             if Patient.objects.filter(patient_id=patient_id).update(access_token=access_token) == 1:
-                return HttpResponse(simplejson.dumps({ "success" : "Patient updated with access token" }),
-                                                     mimetype='application/json')
+                return _generate_response({ "success" : "Patient updated with access token" })
             else:
-                return HttpResponse(simplejson.dumps({ "error" : "Patient id does not exist!" }), mimetype='application/json')
+                return _generate_response({ "error" : "Patient id does not exist!" })
         except KeyError as e:
-            return HttpResponse(simplejson.dumps({ "error" : "Malformed data!", "message" : e }), mimetype='application/json')
+            return _generate_response({ "error" : "Malformed data!", "message" : e })
     else:
-        return HttpResponse(simplejson.dumps({ "error" : "Expecting a POST request" }), mimetype='application/json')
+        return _generate_response({ "error" : "Expecting a POST request" })
 
 
 def logout_patient(request):
@@ -86,14 +85,13 @@ def logout_patient(request):
         try:
             patient_id = json_data['patient_id']
             if Patient.objects.filter(patient_id=patient_id).update(access_token='') == 1:
-                return HttpResponse(simplejson.dumps({ "success" : "Access token removed from patient" }),
-                                    mimetype='application/json')
+                return _generate_response({ "success" : "Access token removed from patient" })
             else:
-                return HttpResponse(simplejson.dumps({ "error" : "Patient id does not exist!" }))
+                return _generate_response({ "error" : "Patient id does not exist!" })
         except KeyError as e:
-            return HttpResponse(simplejson.dumps({ "error" : "Malformed data!", "message" : e }))
+            return _generate_response({ "error" : "Malformed data!", "message" : e })
     else:
-        return HttpResponse(simplejson.dumps({ "error" : "Expecting a POST request" }))
+        return _generate_response({ "error" : "Expecting a POST request" })
 
 
 def create_patient(request):
@@ -103,24 +101,18 @@ def create_patient(request):
         try:
             patient_id = json_data['patient_id']
             access_token = json_data['access_token']
-            administrator_username = None
-            if 'administrator_username' in json_data:
-                administrator_username = json_data['administrator_username']
             if Patient.objects.filter(patient_id=patient_id).update(access_token=access_token) == 1:
-                return HttpResponse(simplejson.dumps({ "success" : "Patient already exists. Updated with access token" }),
-                                                     mimetype='application/json')
+                return _generate_response({ "success" : "Patient already exists. Updated with access token" })
             else:
+                administrator_id = AccessToken.objects.filter(token=access_token)[0].user.id
                 patient = Patient(patient_id=patient_id, access_token=access_token)
                 patient.save()
-                if administrator_username:
-                    administrator_id = User.objects.get(username=administrator_username).id
-                    patient.administrators.add(administrator_id)
-                return HttpResponse(simplejson.dumps({ "success" : "Patient created with access token" }),
-                                                     mimetype='application/json')
+                patient.administrators.add(administrator_id)
+                return _generate_response({ "success" : "Patient created with access token" })
         except KeyError as e:
-            return HttpResponse(simplejson.dumps({ "error" : "Malformed data!", "message" : e }), mimetype='application/json')
+            return _generate_response({ "error" : "Malformed data!", "message" : e })
     else:
-        return HttpResponse(simplejson.dumps({ "error" : "Expecting a POST request" }), mimetype='application/json')
+        return _generate_response({ "error" : "Expecting a POST request" })
 
 
 def get_questions(request, access_token=None, patient_id=None):
@@ -129,19 +121,21 @@ def get_questions(request, access_token=None, patient_id=None):
         patient = Patient.objects.filter(patient_id=patient_id)
 
         if not patient:
-            return HttpResponse(simplejson.dumps({ "error" : "Patient id not found" }))
+            return _generate_response({ "error" : "Patient id not found" })
         else:
             patient = patient[0]
 
         if not patient.access_token:
-            return HttpResponse(simplejson.dumps({ "error" : "Access token not set for patient" }))
+            return_generate_response({ "error" : "Access token not set for patient" })
         if patient.access_token != access_token:
-            return HttpResponse(simplejson.dumps({ "error" : "Access token not valid for patient" }))
+            return _generate_response({ "error" : "Access token not valid for patient" })
 
         # TODO: get the correct survey here
         questions = Question.objects.all()
         data = serializers.serialize('json', questions, indent=2, relations=('multiple_choice_answer',))
         return HttpResponse(data, mimetype='application/json')
     else:
-        return HttpResponse(simplejson.dumps({ "error" : "Expecting a GET request" }))
+        return _generate_response({ "error" : "Expecting a GET request" })
 
+def _generate_response(data):
+    return HttpResponse(simplejson.dumps(data), mimetype='application/json')
